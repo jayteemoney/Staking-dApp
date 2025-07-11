1398f83c61b1e56974a5c6c6ede33a67b0625f1d
jayteemoney <jethroirmiya@gmail.com>
2025-07-11 13:33:24 +0000

    adjusted the unstakebtn

--- src/components/UnstakeButton.jsx
+++ src/components/UnstakeButton.jsx
@@ -2,7 +2,7 @@ function UnstakeButton({ unstakeEth }) {
   return (
     <button
       onClick={unstakeEth}
-      className="w-full bg-red-200 hover:bg-blue-800 text-white py-2 px-4 rounded"
+      className="w-full bg-red-300 hover:bg-red-400 text-white py-2 px-4 rounded"
     >
       Unstake ETH
     </button>
